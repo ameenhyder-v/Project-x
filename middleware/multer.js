86c98313f0c67f76cf1b37bf3b8d05cd14f5aee2
ameenhyder-v <ameenhyder.v@gmail.com>
2024-10-15 11:38:04 +0530

    changed the partials

--- middleware/multer.js
+++ middleware/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer")
 const path = require("path")
 
 const storage = multer.diskStorage({
-    destination: "images",
+    destination: "root/images",
     filename: (req, file, cb) => {
         const ext = path.extname(file.originalname);
         const uniqueFilename = `image_${getRandomThreeDigitNumber()}_${Date.now()}${ext}`
