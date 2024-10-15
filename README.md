ef6be0a01d3cf4558dedf7151a550957732a7be7
ameenhyder-v <ameenhyder.v@gmail.com>
2024-10-15 20:30:46 +0530

    change in multer

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Project-z
 # Project-x
 # This is a simple e-commerce app
+
+This is a ecommerce web app created using MongoDB, Node.Js, Express.Js and EJS.
+I created this project when i am learning node.js and MongoDB as well 
--- middleware/multer.js
+++ middleware/multer.js
@@ -2,13 +2,14 @@ const multer = require("multer")
 const path = require("path")
 
 const storage = multer.diskStorage({
-    destination: "root/images",
+    destination: "images",
     filename: (req, file, cb) => {
         const ext = path.extname(file.originalname);
         const uniqueFilename = `image_${getRandomThreeDigitNumber()}_${Date.now()}${ext}`
         cb(null, uniqueFilename);
     }
 })
+
 
 function getRandomThreeDigitNumber() {
     return Math.floor(Math.random() * 900) + 100;
